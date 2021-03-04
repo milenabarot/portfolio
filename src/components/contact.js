bcf52520ad7458d6f046795302ed094089711ae1
milenabarot <milenabarot@gmail.com>
2021-03-04 17:40:41 +0000

    Initial build of contact form
    - Set up contact form with functionality
    - Set up portfolio-app folder to build API to allow emails to send on contact form. Installed all relevant packages.

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,9 +1,79 @@
+import { useState, useEffect } from "react";
+import "../styles/contact.scss";
+
 function Contact() {
+  const [contactForm, setContactForm] = useState({
+    name: "",
+    email: "",
+    message: "",
+    disabled: false,
+    emailSent: null,
+  });
+
+  useEffect(() => {});
+
+  const handleChange = (event) => {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    setContactForm({
+      ...contactForm,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    setContactForm({ ...contactForm, disabled: true, emailSent: true });
+  };
+
+  console.log(contactForm);
+
   return (
-    <div id="contact">
+    <div className="contact" id="contact">
       <h2>Contact</h2>
-      <input type="text" />
-      <button>Send</button>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={contactForm.name}
+          onChange={handleChange}
+        ></input>
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={contactForm.email}
+          onChange={handleChange}
+        ></input>
+        <label htmlFor="message">Message</label>
+        <textarea
+          className="contact-textArea"
+          type="text"
+          rows="3"
+          id="message"
+          name="message"
+          placeholder="..."
+          form="usrform"
+          value={contactForm.message}
+          onChange={handleChange}
+        ></textarea>
+        <button type="submit" disabled={contactForm.disabled}>
+          Send
+        </button>
+
+        {contactForm.emailSent === true && (
+          <p className="contact-successMessage">Email sent!</p>
+        )}
+        {contactForm.emailSent === false && (
+          <p className="contact-errorMessage">Email not sent!</p>
+        )}
+      </form>
     </div>
   );
 }
