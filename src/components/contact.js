300feefe28b1513bb5a528b4cf0213b5df8535b2
Milena Barot <65911987+milenabarot@users.noreply.github.com>
2021-03-05 15:09:27 +0000

    Contact form
    * Initial build of contact form
    
    - Set up contact form with functionality
    - Set up portfolio-app folder to build API to allow emails to send on contact form. Installed all relevant packages.
    
    * Added send email functionality and nodemon
    
    * Removed node modules

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,9 +1,85 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import "../styles/contact.scss";
+
 function Contact() {
+  const [contactForm, setContactForm] = useState({
+    name: "",
+    email: "",
+    message: "",
+    disabled: false,
+    emailSent: null,
+  });
+
+  const handleChange = (event) => {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    setContactForm({
+      ...contactForm,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    axios
+      .post("http://localhost:3030/api/email", contactForm)
+      .then((res) => {
+        setContactForm({ ...contactForm, disabled: false, emailSent: true });
+      })
+      .catch((err) => {
+        setContactForm({ ...contactForm, disabled: false, emailSent: false });
+      });
+  };
+
+  console.log(contactForm);
+
   return (
-    <div id="contact">
+    <div className="contact" id="contact">
       <h2>Contact</h2>
-      <input type="text" />
-      <button>Send</button>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={contactForm.name}
+          onChange={handleChange}
+        ></input>
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={contactForm.email}
+          onChange={handleChange}
+        ></input>
+        <label htmlFor="message">Message</label>
+        <textarea
+          className="contact-textArea"
+          type="text"
+          rows="3"
+          id="message"
+          name="message"
+          placeholder="..."
+          form="usrform"
+          value={contactForm.message}
+          onChange={handleChange}
+        ></textarea>
+        <button type="submit" disabled={contactForm.disabled}>
+          Send
+        </button>
+
+        {contactForm.emailSent === true && (
+          <p className="contact-successMessage">Email sent!</p>
+        )}
+        {contactForm.emailSent === false && (
+          <p className="contact-errorMessage">Email not sent!</p>
+        )}
+      </form>
     </div>
   );
 }
