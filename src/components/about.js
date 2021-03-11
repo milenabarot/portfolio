fc176f2d4bcbf14954b9264ddc854cd7a18da62a
milenabarot <milenabarot@gmail.com>
2021-03-11 15:44:55 +0000

    Basic styling for About section

--- src/components/about.js
+++ src/components/about.js
@@ -1,9 +1,13 @@
 import "../styles/about.scss";
+import profilepic from "../images/profilepic.jpg";
 
 function About() {
   return (
     <div id="about" className="about">
-      <h2 className="about-title">Bio</h2>
+      <h2 className="about-title">About</h2>
+      <div className="about-image">
+        <img src={profilepic} alt="My profile picture" />
+      </div>
       <p className="about-info">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eu
         augue purus. Aenean et semper lorem, sit amet dictum lectus.
