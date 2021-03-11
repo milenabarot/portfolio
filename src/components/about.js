9bbb5c62604db2c0c4a98006fae94dd8d3ed5d34
milenabarot <milenabarot@gmail.com>
2021-03-11 16:58:31 +0000

    Functionality to show the About component on scroll

--- src/components/about.js
+++ src/components/about.js
@@ -1,7 +1,38 @@
 import "../styles/about.scss";
 import profilepic from "../images/profilepic.jpg";
+import { useEffect, useState } from "react";
 
 function About() {
+  const [isAboutComponentVisible, setIsAboutComponentVisible] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", (event) => {
+      shouldShowAboutComponent();
+    });
+  }, []);
+
+  // scroll function to calculate when to show About component
+
+  const shouldShowAboutComponent = () => {
+    const scrollTop = window.pageYOffset;
+    const aboutElement = document.querySelector("#about");
+    const headerElement = document.querySelector("#header");
+    const projectsElement = document.querySelector("#projects");
+
+    const aboutTotalDistanceFromTop = aboutElement.offsetHeight;
+    const headerTotalDistanceFromTop = headerElement.offsetHeight;
+    const projectsTotalDistanceFromTop = projectsElement.offsetHeight;
+
+    const distanceNeeded =
+      headerTotalDistanceFromTop +
+      projectsTotalDistanceFromTop -
+      aboutTotalDistanceFromTop;
+
+    if (scrollTop >= distanceNeeded) {
+      setIsAboutComponentVisible(true);
+    }
+  };
+
   return (
     <div id="about" className="about">
       <h2 className="about-title">About</h2>
