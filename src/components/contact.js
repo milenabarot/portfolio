0ba9da9b8c9075f701f6b0736990537f7bbf5897
milenabarot <milenabarot@gmail.com>
2021-03-12 12:27:16 +0000

    Contact form styling

--- src/components/contact.js
+++ src/components/contact.js
@@ -39,45 +39,51 @@ function Contact() {
 
   return (
     <div className="contact" id="contact">
-      <h2>Contact</h2>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="name">Name</label>
+      <h2 className="contact-title">Contact</h2>
+      <form onSubmit={handleSubmit} className="contact-form">
+        <label htmlFor="name"></label>
         <input
           type="text"
           id="name"
           name="name"
           value={contactForm.name}
           onChange={handleChange}
+          placeholder="Name"
         ></input>
-        <label htmlFor="email">Email</label>
+        <label htmlFor="email"></label>
         <input
           type="email"
           id="email"
           name="email"
           value={contactForm.email}
           onChange={handleChange}
+          placeholder="Email"
         ></input>
-        <label htmlFor="message">Message</label>
+        <label htmlFor="message"></label>
         <textarea
-          className="contact-textArea"
+          className="contact-form-textArea"
           type="text"
           rows="3"
           id="message"
           name="message"
-          placeholder="..."
+          placeholder="Message..."
           form="usrform"
           value={contactForm.message}
           onChange={handleChange}
         ></textarea>
-        <button type="submit" disabled={contactForm.disabled}>
+        <button
+          className="contact-form-button"
+          type="submit"
+          disabled={contactForm.disabled}
+        >
           Send
         </button>
 
         {contactForm.emailSent === true && (
-          <p className="contact-successMessage">Email sent!</p>
+          <p className="contact-form-successMessage">Email sent!</p>
         )}
         {contactForm.emailSent === false && (
-          <p className="contact-errorMessage">Email not sent!</p>
+          <p className="contact-form-errorMessage">Email not sent!</p>
         )}
       </form>
     </div>
